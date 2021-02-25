bb6b397e6c5be7d62a3923d99a8fa04c2b4febba
Kate <test@gmail.com>
2021-02-25 10:45:38 -0500

    error service

---
+++ worker/src/crawler/error.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { CrawlerError, CrawlerErrorDoc } from 'mongo/schemas';
+import { Model } from 'mongoose';
+
+export interface IErrorInfo {
+  type: string;
+  error: unknown;
+  url?: string;
+  params?: Record<string, string>;
+}
+
+@Injectable()
+export class ErrorService {
+  private logger = new Logger('ErrorService');
+  constructor(
+    @InjectModel(CrawlerError.name)
+    private readonly errorModel: Model<CrawlerErrorDoc>,
+  ) {}
+
+  /**
+   * Track error
+   * @param{IErrorInfo} errorInfo the error information
+   * @param{boolean}  useLog log error information to terminal?
+   */
+  logError(errorInfo: IErrorInfo, useLog?: boolean) {
+    const { params, error, ...rest } = errorInfo;
+    if (useLog) {
+      this.logger.log(`Error:`);
+      this.logger.log(error);
+      this.logger.log(`\n`);
+    }
+    return this.errorModel.create({
+      ...rest,
+      error: JSON.stringify(error),
+      params: JSON.stringify(params),
+    });
+  }
+}
