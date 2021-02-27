a4c939ccbb3070c0c2023b47b0276fe3875b3afa
Kate <test@gmail.com>
2021-02-27 09:21:14 -0500

    fix routing bug

--- frontend/src/context/index.js
+++ frontend/src/context/index.js
@@ -2,42 +2,30 @@
 import styled from "styled-components/macro";
 import GlobalStyles from "components/Globalstyles";
 import Header from "components/header";
-import { Content, ThemedElement } from "components/lib";
+import { Content } from "components/lib";
 import * as React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 
 export default function AppProvider({ children }) {
-  const client = new QueryClient();
-  return (
-    <QueryClientProvider client={client}>
-      <GlobalStyles />
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Content
-            as="main"
-            css={`
-              padding-bottom: 10vh;
-            `}
-          >
-            {children}
-            <Route
-              component={() => (
-                <ThemedElement
-                  css={`
-                    padding: 2rem 0;
-                    text-align: center;
-                  `}
-                >
-                  <h2>Can't find this page.</h2>
-                </ThemedElement>
-              )}
-              exact
-            />
-          </Content>
-        </Switch>
-      </BrowserRouter>
-    </QueryClientProvider>
-  );
+	const client = new QueryClient();
+	return (
+		<QueryClientProvider client={client}>
+			<GlobalStyles />
+			<BrowserRouter>
+				<Header />
+				<Content
+					as="main"
+					css={`
+						padding-bottom: 10vh;
+					`}
+				>
+					<Switch>
+						{children}
+						<Route component={() => <Redirect to="/" />} exact />
+					</Switch>
+				</Content>
+			</BrowserRouter>
+		</QueryClientProvider>
+	);
 }
