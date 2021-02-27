840f20273ad00d9b44eec2a8176502a2854ad03a
Kate <test@gmail.com>
2021-02-27 08:01:03 -0500

    handle the condition that challenge is not exist

--- frontend/src/screens/challenge/components/ChallengeInfo/index.js
+++ frontend/src/screens/challenge/components/ChallengeInfo/index.js
@@ -2,48 +2,57 @@
 import styled from "styled-components/macro";
 import * as React from "react";
 import {
-  ChallengeName,
-  InfoItem,
-  InfoItemName,
-  ChallengeIntro,
-  Wrapper,
-  ChallengeImg,
+	ChallengeName,
+	InfoItem,
+	InfoItemName,
+	ChallengeIntro,
+	Wrapper,
+	ChallengeImg,
 } from "./styles";
 
-export default function Challenge({ ...challenge }) {
-  if(!challenge) return null;
-  return (
-    <Wrapper
-      href={`https://www.frontendmentor.io/challenges/${challenge.slug}`}
-    >
-      <ChallengeImg
-        css={`
-          background-image: url(${challenge.heroImage});
-        `}
-      />
-      <ChallengeIntro>
-        <ChallengeName>{challenge.title}</ChallengeName>
-        <InfoItem>
-          <InfoItemName>Languages:</InfoItemName>
-          {challenge.languages.join(",")}
-        </InfoItem>
-        <InfoItem
-          css={`
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-          `}
-        >
-          <span>
-            <InfoItemName>Difficulty:</InfoItemName>
-            {challenge.difficulty}
-          </span>
-          <span>
-            <InfoItemName>Solutions:</InfoItemName>
-            {challenge.solutions}
-          </span>
-        </InfoItem>
-      </ChallengeIntro>
-    </Wrapper>
-  );
+export default function Challenge({ challenge }) {
+	if (!challenge)
+		return (
+			<Wrapper
+				css={`
+					justify-content: center;
+				`}
+			>
+				<h2>Challenge does not exist </h2>
+			</Wrapper>
+		);
+	return (
+		<Wrapper
+			href={`https://www.frontendmentor.io/challenges/${challenge.slug}`}
+		>
+			<ChallengeImg
+				css={`
+					background-image: url(${challenge.heroImage});
+				`}
+			/>
+			<ChallengeIntro>
+				<ChallengeName>{challenge.title}</ChallengeName>
+				<InfoItem>
+					<InfoItemName>Languages:</InfoItemName>
+					{challenge.languages.join(",")}
+				</InfoItem>
+				<InfoItem
+					css={`
+						display: flex;
+						align-items: center;
+						justify-content: space-between;
+					`}
+				>
+					<span>
+						<InfoItemName>Difficulty:</InfoItemName>
+						{challenge.difficulty}
+					</span>
+					<span>
+						<InfoItemName>Solutions:</InfoItemName>
+						{challenge.solutions}
+					</span>
+				</InfoItem>
+			</ChallengeIntro>
+		</Wrapper>
+	);
 }
--- frontend/src/screens/challenge/index.js
+++ frontend/src/screens/challenge/index.js
@@ -7,22 +7,20 @@ import ChallengeSolutions from "./components/ChallengeSolutions";
 import { Wrapper } from "./styles";
 
 export default function ChallengeScreen() {
-  const { femId } = useParams();
-  const { data: challenge, status, error } = useChallenge(femId);
-  const [total, setTotal] = React.useState(0);
-  React.useEffect(() => {
-    if (challenge) {
-      setTotal(challenge.solutions);
-    }
-  }, [challenge]);
-  return (
-    <Wrapper>
-      {status === "loading" ? <Spinner /> : null}
-      {status === "failed" ? <p>{JSON.stringify(error)}</p> : null}
-      {status === "success" && challenge ? (
-        <ChallengeInfo {...challenge} />
-      ) : null}
-      <ChallengeSolutions femId={femId} total={total} />
-    </Wrapper>
-  );
+	const { femId } = useParams();
+	const { data: challenge, status, error } = useChallenge(femId);
+	const [total, setTotal] = React.useState(0);
+	React.useEffect(() => {
+		if (challenge) {
+			setTotal(challenge.solutions);
+		}
+	}, [challenge]);
+	return (
+		<Wrapper>
+			{status === "loading" ? <Spinner /> : null}
+			{status === "failed" ? <p>{JSON.stringify(error)}</p> : null}
+			{status === "success" ? <ChallengeInfo challenge={challenge} /> : null}
+			<ChallengeSolutions femId={femId} total={total} />
+		</Wrapper>
+	);
 }
