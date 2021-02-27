f7f42ddd0ce7b62fb422dbc515388df8401ad618
Kate <test@gmail.com>
2021-02-26 19:19:02 -0500

    add logger middleware

--- backend/src/api/api.module.ts
+++ backend/src/api/api.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { LoggerMiddleware } from 'common/middlewares';
 import { ChallengesModule } from './challenges/challenges.module';
 import { SolutionsModule } from './solutions/solutions.module';
 import { UsersModule } from './users/users.module';
@@ -6,4 +7,8 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [ChallengesModule, SolutionsModule, UsersModule],
 })
-export class ApiModule {}
+export class ApiModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('/v1');
+  }
+}
