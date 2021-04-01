f978e6ce2eeb7b7935a91f76d03d63fc93092063
Kate <3837437+katesroad@users.noreply.github.com>
2021-04-01 15:36:08 -0400

    Update index.js

--- frontend/src/screens/challenge/index.js
+++ frontend/src/screens/challenge/index.js
@@ -11,57 +11,57 @@ import ChallengeInfo from "./components/ChallengeInfo";
 import ChallengeSolutions from "./components/ChallengeSolutions";
 
 export default function ChallengeScreen() {
-	const history = useHistory();
-	const { femId } = useParams();
-	const { data: challenge, status, error } = useChallenge(femId);
-	const [total, setTotal] = React.useState(0);
-	React.useEffect(() => {
-		if (challenge) {
-			setTotal(challenge.solutions);
-		}
-	}, [challenge]);
-	const onSearch = (challengeName) => {
-		history.push({
-			search: `search=${encodeURIComponent(challengeName)}`,
-			pathname: "/",
-		});
-	};
-	return (
-		<>
-			<AppHeader>
-				<NameFilter
-					onSearch={onSearch}
-					css={`
-						display: none;
-						box-shadow: none !important;
-						font-size: 14px;
-						input:focus {
-							border-radius: 6px;
-							border: 1px solid var(--shadow);
-						}
-						${mediaQueries.small} {
-							display: flex;
-						}
-					`}
-				/>
-			</AppHeader>
-			<Content
-				as="main"
-				css={`
-					${mediaQueries.large} {
-						display: grid;
-						grid-template-columns: 4fr 2fr;
-						grid-auto-rows: minmax(380px, auto);
-						gap: 2.5rem;
-						padding-bottom: 2rem;
-					}
-				`}
-			>
-				{status === "loading" ? <Spinner /> : null}
-				{status === "failed" ? <p>{JSON.stringify(error)}</p> : null}
-				{status === "success" ? <ChallengeInfo challenge={challenge} /> : null}
-				<ChallengeSolutions femId={femId} total={total} />
-			</Content>
-		</>
-	);
+  const history = useHistory();
+  const { femId } = useParams();
+  const { data: challenge, status, error } = useChallenge(femId);
+  const [total, setTotal] = React.useState(0);
+  React.useEffect(() => {
+    if (challenge) {
+      setTotal(challenge.solutions);
+    }
+  }, [challenge]);
+  const onSearch = (challengeName) => {
+    history.push({
+      search: `search=${encodeURIComponent(challengeName)}`,
+      pathname: "/",
+    });
+  };
+  return (
+    <>
+      <AppHeader>
+        <NameFilter
+          onSearch={onSearch}
+          css={`
+            display: none;
+            box-shadow: none !important;
+            font-size: 14px;
+            input:focus {
+              border-radius: 6px;
+              border: 1px solid var(--shadow);
+            }
+            ${mediaQueries.small} {
+              display: flex;
+            }
+          `}
+        />
+      </AppHeader>
+      <Content
+        as="main"
+        css={`
+          ${mediaQueries.large} {
+            display: grid;
+            grid-template-columns: 4fr 2fr;
+            grid-auto-rows: minmax(380px, auto);
+            gap: 2.5rem;
+            padding-bottom: 2rem;
+          }
+        `}
+      >
+        {status === "loading" ? <Spinner /> : null}
+        {status === "failed" ? <p>{JSON.stringify(error)}</p> : null}
+        {status === "success" ? <ChallengeInfo challenge={challenge} /> : null}
+        <ChallengeSolutions femId={femId} total={total} />
+      </Content>
+    </>
+  );
 }
