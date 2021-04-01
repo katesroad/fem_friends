f48f7a541bba261945b3eea1de800e5442944a5b
Kate <3837437+katesroad@users.noreply.github.com>
2021-04-01 15:38:04 -0400

    Update index.js

--- frontend/src/screens/home/components/Challenge/index.js
+++ frontend/src/screens/home/components/Challenge/index.js
@@ -3,50 +3,50 @@ import styled from "styled-components/macro";
 import * as React from "react";
 import { Link } from "react-router-dom";
 import {
-	ChallengeName,
-	InfoItem,
-	InfoItemName,
-	ChallengeIntro,
+  ChallengeName,
+  InfoItem,
+  InfoItemName,
+  ChallengeIntro,
 } from "./styles";
 
 export default function Challenge({ ...challenge }) {
-	return (
-		<Link
-			to={`/challenge/${challenge.id}`}
-			css={`
-				display: block;
-				height: 100%;
-				color: var(--text-color);
-				background-color: var(--elements-background);
-				overflow: hidden;
-				border-radius: 0.5rem;
-				box-shadow: 0 3px 4px var(--shadow);
-			`}
-		>
-			<img src={challenge.heroImage} alt={challenge.title} />
-			<ChallengeIntro>
-				<ChallengeName>{challenge.title}</ChallengeName>
-				<InfoItem>
-					<InfoItemName>Languages:</InfoItemName>
-					{challenge.languages.join(",")}
-				</InfoItem>
-				<InfoItem
-					css={`
-						display: flex;
-						justify-content: space-between;
-						align-items: center;
-					`}
-				>
-					<span>
-						<InfoItemName>Difficulty:</InfoItemName>
-						{challenge.difficulty}
-					</span>
-					<span>
-						<InfoItemName>Solutions:</InfoItemName>
-						{challenge.solutions}
-					</span>
-				</InfoItem>
-			</ChallengeIntro>
-		</Link>
-	);
+  return (
+    <Link
+      to={`/challenge/${challenge.id}`}
+      css={`
+        display: block;
+        height: 100%;
+        color: var(--text-color);
+        background-color: var(--elements-background);
+        overflow: hidden;
+        border-radius: 0.5rem;
+        box-shadow: 0 3px 4px var(--shadow);
+      `}
+    >
+      <img src={challenge.heroImage} alt={challenge.title} />
+      <ChallengeIntro>
+        <ChallengeName>{challenge.title}</ChallengeName>
+        <InfoItem>
+          <InfoItemName>Languages:</InfoItemName>
+          {challenge.languages.join(",")}
+        </InfoItem>
+        <InfoItem
+          css={`
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+          `}
+        >
+          <span>
+            <InfoItemName>Difficulty:</InfoItemName>
+            {challenge.difficulty}
+          </span>
+          <span>
+            <InfoItemName>Solutions:</InfoItemName>
+            {challenge.solutions}
+          </span>
+        </InfoItem>
+      </ChallengeIntro>
+    </Link>
+  );
 }
