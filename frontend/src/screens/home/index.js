0896b946e2ec9fe23c2e61dc8544b09031e518d9
Kate <test@gmail.com>
2021-04-22 16:03:02 -0400

    replace with global spiner

--- frontend/src/screens/home/index.js
+++ frontend/src/screens/home/index.js
@@ -13,108 +13,127 @@ import { useHistory, useLocation } from "react-router-dom";
 import * as qs from "query-string";
 
 export default function HomeScreen() {
-	const location = useLocation();
-	const history = useHistory();
-	const [search, setSearch] = React.useState(() => {
-		try {
-			return qs.parse(location.search).search;
-		} catch (e) {
-			return "";
-		}
-	});
-	const { status, error, data } = useChallenges();
-	const onSearch = (search) => setSearch(search);
-	const [challenges, setChallenges] = React.useState([]);
+  const location = useLocation();
+  const history = useHistory();
+  const [search, setSearch] = React.useState(() => {
+    try {
+      return qs.parse(location.search).search;
+    } catch (e) {
+      return "";
+    }
+  });
+  const { status, error, data } = useChallenges();
+  const onSearch = (search) => setSearch(search);
+  const [challenges, setChallenges] = React.useState([]);
 
-	const queryClient = useQueryClient();
-	React.useEffect(() => {
-		const cacheData = queryClient.getQueryData("challenges") || [];
-		let result = cacheData;
-		if (search) {
-			result = matchSorter(cacheData || [], search, { keys: ["title"] });
-		} else {
-			result = cacheData;
-		}
-		setChallenges(result);
-		// update query string in brower URL bar
-		const params = { pathname: "/" };
-		if (search) {
-			params.search = `search=${encodeURIComponent(search)}`;
-		}
-		history.replace(params);
-	}, [data, search]);
+  const queryClient = useQueryClient();
+  React.useEffect(() => {
+    const cacheData = queryClient.getQueryData("challenges") || [];
+    let result = cacheData;
+    if (search) {
+      result = matchSorter(cacheData || [], search, { keys: ["title"] });
+    } else {
+      result = cacheData;
+    }
+    setChallenges(result);
+    // update query string in brower URL bar
+    const params = { pathname: "/" };
+    if (search) {
+      params.search = `search=${encodeURIComponent(search)}`;
+    }
+    history.replace(params);
+  }, [data, search]);
 
-	if (["idle", "loading"].includes(status)) return <Spinner />;
-	if (status === "failed") return <p>{JSON.stringify(error)}</p>;
+  if (["idle", "loading"].includes(status)) {
+    return (
+      <div
+        css={`
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100vw;
+          height: 100vh;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-size: 2rem;
+        `}
+      >
+        <Spinner />
+      </div>
+    );
+  }
 
-	return (
-		<>
-			<AppHeader />
-			<Content
-				as="main"
-				css={`
-					padding-bottom: 10vh;
-				`}
-			>
-				<div
-					css={`
-						display: flex;
-						align-items: center;
-						justify-content: space-between;
-						flex-wrap: wrap;
-						margin-bottom: calc(4vw + 3rem);
-						${mediaQueries.medium} {
-							margin-bottom: 4rem;
-						}
-					`}
-				>
-					<h2
-						css={`
-							display: none;
-							line-height: 1;
-							${mediaQueries.medium} {
-								display: inline-block;
-							}
-						`}
-					>
-						FEM challenges
-					</h2>
-					<div
-						css={`
-							display: flex;
-							justify-content: space-between;
-							align-items: center;
-							flex-wrap: wrap;
-							width: 100%;
-							${mediaQueries.medium} {
-								width: auto;
-							}
-						`}
-					>
-						<NameFilter name={search} onSearch={onSearch} />
-					</div>
-				</div>
-				<ul
-					css={`
-						width: 100%;
-						display: grid;
-						grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-						grid-auto-rows: minmax(380px, auto);
-						gap: 2.5rem;
-						padding-bottom: 2rem;
-					`}
-				>
-					{challenges.length ? (
-						challenges.map((challenge) => (
-							<li key={challenge.id}>
-								<Challenge {...challenge} />
-							</li>
-						))
-					) : (
-						<p>No data with given challenge name {search}</p>
-					)}
-				</ul>
-			</Content>
-		</>
-	);
+  if (status === "failed") return <p>{JSON.stringify(error)}</p>;
+
+  return (
+    <>
+      <AppHeader />
+      <Content
+        as="main"
+        css={`
+          padding-bottom: 10vh;
+        `}
+      >
+        <div
+          css={`
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            flex-wrap: wrap;
+            margin-bottom: calc(4vw + 3rem);
+            ${mediaQueries.medium} {
+              margin-bottom: 4rem;
+            }
+          `}
+        >
+          <h2
+            css={`
+              display: none;
+              line-height: 1;
+              ${mediaQueries.medium} {
+                display: inline-block;
+              }
+            `}
+          >
+            FEM challenges
+          </h2>
+          <div
+            css={`
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+              flex-wrap: wrap;
+              width: 100%;
+              ${mediaQueries.medium} {
+                width: auto;
+              }
+            `}
+          >
+            <NameFilter name={search} onSearch={onSearch} />
+          </div>
+        </div>
+        <ul
+          css={`
+            width: 100%;
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+            grid-auto-rows: minmax(380px, auto);
+            gap: 2.5rem;
+            padding-bottom: 2rem;
+          `}
+        >
+          {challenges.length ? (
+            challenges.map((challenge) => (
+              <li key={challenge.id}>
+                <Challenge {...challenge} />
+              </li>
+            ))
+          ) : (
+            <p>No data with given challenge name {search}</p>
+          )}
+        </ul>
+      </Content>
+    </>
+  );
 }
