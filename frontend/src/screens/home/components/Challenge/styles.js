37eb26dfd6c34e793efb41c40a350996333efba5
Kate <test@gmail.com>
2021-04-22 17:00:03 -0400

    format code

--- frontend/src/screens/home/components/Challenge/styles.js
+++ frontend/src/screens/home/components/Challenge/styles.js
@@ -2,30 +2,30 @@
 import styled from "styled-components/macro";
 
 const ChallengeName = styled.h3`
-	margin-top: 0.5rem;
-	margin-bottom: 1.25rem;
-	font-weight: var(--weight-bolder);
+  margin-top: 0.5rem;
+  margin-bottom: 1.25rem;
+  font-weight: var(--weight-bolder);
 `;
 
 const InfoItem = styled.p`
-	display: flex;
-	align-items: center;
-	flex-wrap: wrap;
-	margin-bottom: 1rem;
-	font-size: 0.9rem;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-bottom: 1rem;
+  font-size: 0.9rem;
 `;
 
 const InfoItemName = styled.strong`
-	margin-right: 0.4rem;
-	font-weight: var(--weight-bolder);
-	text-transform: capitalize;
-	&::nth-child(2) {
-		margin-left: 0.4rem;
-	}
+  margin-right: 0.4rem;
+  font-weight: var(--weight-bolder);
+  text-transform: capitalize;
+  &::nth-child(2) {
+    margin-left: 0.4rem;
+  }
 `;
 
 const ChallengeIntro = styled.div`
-	padding: 1rem 1.5rem;
+  padding: 1rem 1.5rem;
 `;
 
 export { ChallengeName, InfoItem, InfoItemName, ChallengeIntro };
